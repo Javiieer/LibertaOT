71502276e9bcd1a8cdd64b7a5c1af7b6c3aa4253
ShyCloud <uran.animate2@gmail.com>
2020-02-12 20:09:29 -0600

    Traduccion Del Ingles al Español
    Cambio no tan importante

--- globalevents/scripts/serversave.lua
+++ globalevents/scripts/serversave.lua
@@ -16,17 +16,17 @@ local function serverSave()
 end
 
 local function secondServerSaveWarning()
-	--Game.broadcastMessage("Server is saving game in one minute. Please logout.", MESSAGE_EVENT_ADVANCE)
+	--Game.broadcastMessage("Guardando El Servidor En Un Minuto. Please logout.", MESSAGE_EVENT_ADVANCE)
 	addEvent(serverSave, 60000)
 end
 
 local function firstServerSaveWarning()
-	--Game.broadcastMessage("Server is saving game in 3 minutes. Please logout.", MESSAGE_EVENT_ADVANCE)
+	--Game.broadcastMessage("Guardando El Servidor en 3 minutos. Please logout.", MESSAGE_EVENT_ADVANCE)
 	addEvent(secondServerSaveWarning, 120000)
 end
 
 function onTime(interval)
-	--Game.broadcastMessage("Server is saving game in 5 minutes. Please logout.", MESSAGE_EVENT_ADVANCE)
+	--Game.broadcastMessage("Guardando El Servidor en 5 minutos. Please logout.", MESSAGE_EVENT_ADVANCE)
 	addEvent(firstServerSaveWarning, 120000)
 	return not shutdownAtServerSave
 end
